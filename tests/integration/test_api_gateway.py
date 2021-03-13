47bcac29472ee5081cf547de1c572eddbd1b8ca9
Raniendu Singh <raniendu.work@gmail.com>
2021-03-12 22:36:24 -0800

    Checkpoint

--- tests/integration/test_api_gateway.py
+++ tests/integration/test_api_gateway.py
@@ -42,8 +42,8 @@ class TestApiGateway(TestCase):
         stacks = response["Stacks"]
 
         stack_outputs = stacks[0]["Outputs"]
-        api_outputs = [output for output in stack_outputs if output["OutputKey"] == "HelloWorldApi"]
-        self.assertTrue(api_outputs, f"Cannot find output HelloWorldApi in stack {stack_name}")
+        api_outputs = [output for output in stack_outputs if output["OutputKey"] == "BabaMuskApi"]
+        self.assertTrue(api_outputs, f"Cannot find output BabaMuskApi in stack {stack_name}")
 
         self.api_endpoint = api_outputs[0]["OutputValue"]
 
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -2,7 +2,7 @@ import json
 
 import pytest
 
-from baba_musk_bot import app
+from ...baba_musk_bot import app
 
 
 @pytest.fixture()
